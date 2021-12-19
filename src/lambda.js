4f457394a7982b3859c0660dda79797900a11e93
Akin Palmer <akin@afkas.net>
2021-12-19 22:47:37 +0000

    This is my stack demo

--- src/lambda.js
+++ src/lambda.js
@@ -2,6 +2,6 @@ export async function handler(event) {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: `I hate Hello, World Applications! Your request was received at ${event.requestContext.time}.` ,
+    body: `I still have a profound hatered for Hello, World Applications! Your request was received at ${event.requestContext.time}.` ,
   };
 }
