edfbb4fe534cf69852e6587a8c62a9614ed0a999
Akin Palmer <akin@afkas.net>
2021-12-21 16:40:17 +0000

    Would not start

--- stacks/index.js
+++ stacks/index.js
@@ -1,9 +1,11 @@
 import StorageStack from './StorageStack';
+import ApiStack from './ApiStack';
 
 export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs12.x"
+
+  const storageStack = new StorageStack(app, "Storage");
+
+  new ApiStack(app, "api", {
+    table: storageStack.table,
   });
-  new StorageStack(app, 'storage');
 }
